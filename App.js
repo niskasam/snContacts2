fc5a1047bdee3e1a1cf31053488e6fbf1de68b1d
niskasam <samnisk@outlook.com>
2021-12-02 21:52:35 +0200

    snContacts2, 2.12.2021

--- App.js
+++ App.js
@@ -1,12 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import * as Contacts from "expo-contacts";
+import { StyleSheet, View, Text, FlatList } from "react-native";
+import { Header, Button, ListItem } from "react-native-elements";
 
 export default function App() {
+  const [contacts, setContacts] = useState([]);
+  const [amount, setAmount] = useState(0);
+
+  const getAllContacts = async () => {
+    console.log("Test");
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === "granted") {
+      const { data } = await Contacts.getContactsAsync({
+        fields: [Contacts.Fields.PhoneNumbers],
+      });
+
+      if (data.length > 0) {
+        setContacts(data);
+        setAmount(data.length);
+      }
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header
+        leftComponent={{ icon: "menu", color: "#fff" }}
+        centerComponent={{
+          text: "Contacts",
+          style: { color: "#fff", fontSize: 18 },
+        }}
+        rightComponent={{
+          icon: "home",
+          color: "#fff",
+        }}
+      />
+
+      <View style={styles.buttonContainer}>
+        <Button
+          buttonStyle={styles.saveButton}
+          raised
+          icon={{ name: "search", color: "white" }}
+          onPress={getAllContacts}
+          title="Get all contacts"
+        />
+      </View>
+
+      <Text h3>{amount} contacts found</Text>
+
+      <View style={styles.flatListContainer}>
+        <FlatList
+          keyExtractor={(item) => item.id}
+          data={contacts}
+          renderItem={({ item }) => (
+            <ListItem bottomDivider>
+              <ListItem.Content>
+                <ListItem.Title>{item.name}</ListItem.Title>
+                <ListItem.Subtitle>
+                  {item.phoneNumbers
+                    ? item.phoneNumbers[0].number
+                    : "No phone number available"}
+                </ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+          )}
+        />
+      </View>
     </View>
   );
 }
@@ -14,8 +72,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
+  },
+
+  flatListContainer: {
+    flex: 1,
+    width: "100%",
+    margin: 0,
+  },
+
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 10,
   },
 });
